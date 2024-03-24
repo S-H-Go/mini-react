82008d1cc35e92f515ae83e2a3fb0a566b16a4d2
shg <wangshijiehh@outlook.com>
2024-03-24 20:40:20 +0800

    V03 init

---
+++ V03/App.jsx
@@ -0,0 +1,45 @@
+/** @jsx MReact.createElement */
+// 使用上面那个 pragma 指定转换方法jsx名称之后，空标签依然使用的是 React.Fragment，而不是 MReact.Fragment。
+// 需要下面这个 pragma 进行替换
+/** @jsxFrag MReact.Fragment*/
+import MReact from './core/React.js'
+
+let msg = '无法更新的语句'
+let num = 0
+
+function increment() {
+  console.log('increment', num)
+  num++
+}
+
+const arr = []
+// i 的范围再多加一个 0 就会栈溢出
+for (let i = 0; i < 100000; i++) {
+  arr.push(<li>{i}</li>)
+}
+// 获取不到 DOM
+console.log(document.querySelector('#btn'))
+const App = (
+  // 空标签实际上是React.Fragment,因为没有定义，所以会渲染为一个 undefined tags
+  <>
+    <div id="app">
+      <p>{msg}</p>
+      <p>{num}</p>
+      {/* 无法赋值上 onClick */}
+      <button onClick={increment}>onClick</button>
+      <button id="btn" className="1234" onclick={increment}>
+        无法更新
+      </button>
+      <ul>{arr}</ul>
+    </div>
+    <div>
+      <span>1 &gt; 0? </span>
+      {1 > 0 ? <span>yes</span> : <span>no</span>}
+    </div>
+  </>
+)
+function AppOne() {
+  return <div id="app">Hello World</div>
+}
+console.log(AppOne)
+export default App
---
+++ V03/core/React.js
@@ -0,0 +1,61 @@
+const Fragment = 0
+
+function createTextNode(nodeValue) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue,
+      children: [],
+    },
+  }
+}
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => {
+        if (typeof child === 'string' || typeof child === 'number') {
+          return createTextNode(child)
+        } else if (Array.isArray(child)) {
+          return createElement(Fragment, null, ...child)
+        } else {
+          return child
+        }
+      }),
+    },
+  }
+}
+
+function render(vdom, container) {
+  let dom
+  // 根据不同的vdom类型创建对应的真实dom
+  if (vdom.type === 'TEXT_ELEMENT') {
+    dom = document.createTextNode('')
+  } else if (vdom.type === Fragment) {
+    dom = document.createDocumentFragment()
+  } else {
+    dom = document.createElement(vdom.type)
+  }
+  // 处理 vdom 的 props
+  Object.keys(vdom.props).forEach((key) => {
+    if (key !== 'children') {
+      dom[key] = vdom.props[key]
+    }
+  })
+  // 处理 vdom 的 children
+  vdom.props.children.forEach((child) => {
+    render(child, dom)
+  })
+  // 在容器中添加 dom
+  container.append(dom)
+}
+
+const React = {
+  createElement,
+  render,
+  Fragment,
+}
+
+export default React
