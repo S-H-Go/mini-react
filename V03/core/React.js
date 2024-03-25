79c32baeaf4b80b352a3ce29e8fcc066b1529203
shg <wangshijiehh@outlook.com>
2024-03-25 09:19:43 +0800

    feat: Implement the frber architecture

--- V03/core/React.js
+++ V03/core/React.js
@@ -29,29 +29,84 @@ function createElement(type, props, ...children) {
 }
 
 function render(vdom, container) {
-  let dom
-  // 根据不同的vdom类型创建对应的真实dom
-  if (vdom.type === 'TEXT_ELEMENT') {
-    dom = document.createTextNode('')
-  } else if (vdom.type === Fragment) {
-    dom = document.createDocumentFragment()
-  } else {
-    dom = document.createElement(vdom.type)
+  nextWorkOfUnit = {
+    dom: container,
+    props: {
+      children: [vdom],
+    },
   }
-  // 处理 vdom 的 props
-  Object.keys(vdom.props).forEach((key) => {
-    if (key !== 'children') {
-      dom[key] = vdom.props[key]
-    }
-  })
-  // 处理 vdom 的 children
-  vdom.props.children.forEach((child) => {
-    render(child, dom)
-  })
-  // 在容器中添加 dom
-  container.append(dom)
 }
 
+let nextWorkOfUnit = {}
+function workLoop(DealLine) {
+  let shouldYield = false
+  while (!shouldYield && nextWorkOfUnit) {
+    nextWorkOfUnit = preformWorkOfUnit(nextWorkOfUnit)
+    shouldYield = DealLine.timeRemaining() < 1
+  }
+
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+function createDom(type) {
+  // 根据不同的vdom类型创建对应的真实dom
+  if (type === 'TEXT_ELEMENT') {
+    return document.createTextNode('')
+  } else if (type === Fragment) {
+    return document.createDocumentFragment()
+  } else {
+    return document.createElement(type)
+  }
+}
+
+function updateProps(dom, props) {
+  Object.keys(props).forEach((key) => {
+    if (key !== 'children') {
+      dom[key] = props[key]
+    }
+  })
+}
+
+function initChildren(fiber) {
+  const children = fiber.props.children
+  let prevChild = null
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: fiber,
+      sibling: null,
+      dom: null,
+    }
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevChild.sibling = newFiber
+    }
+    prevChild = newFiber
+  })
+}
+function preformWorkOfUnit(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber.type)
+
+    fiber.parent.dom.append(fiber.dom)
+
+    updateProps(fiber.dom, fiber.props)
+  }
+  // 转换成链表结构
+  initChildren(fiber)
+  // 返回下一次空闲时间要调度的任务
+  if (fiber.child) {
+    return fiber.child
+  }
+  if (fiber.sibling) {
+    return fiber.sibling
+  }
+  return fiber.parent?.sibling
+}
 const React = {
   createElement,
   render,
